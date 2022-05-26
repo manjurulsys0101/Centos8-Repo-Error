0242320b3792a0b0d67fb74fd0002a682c175252
Manjurul <80325869+manjurulsys0101@users.noreply.github.com>
2022-05-26 11:20:42 +0600

    Update repoerror.sh

--- repoerror.sh
+++ repoerror.sh
@@ -3,6 +3,5 @@ sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 dnf clean all
 dnf swap centos-linux-repos centos-stream-repos -y
-ll
-dnf update
+dnf update -y --allowerasing --nobest
 reboot
